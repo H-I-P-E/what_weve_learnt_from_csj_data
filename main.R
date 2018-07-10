c238126dde744acb68453aae517e4bbb5f915b00
Toby <tobias.jolly@education.gov.uk>
2018-07-10 18:13:51 +0100

    Started project - bare bones

---
+++ main.R
@@ -0,0 +1,19 @@
+library(readr)
+
+
+
+lookups_folder <- 'lookups'
+data_folder <- 'data'
+
+
+competencies_file <- file.path(lookups_folder, 'competencies.csv')
+post_code_locations_file <- file.path(lookups_folder, 'Postcode districts.csv')
+competency_data_file <- file.path(data_folder, 'competency_data.csv')
+cleaned_data_csv <- file.path(data_folder, 'cleaned_advert_data.csv')
+role_data_csv <- file.path(data_folder, 'role_data.csv')
+grade_data_csv <- file.path(data_folder, 'grade_data.csv')
+salary_data_csv <- file.path(data_folder, 'salary_data.csv')
+location_data_csv <- file.path(data_folder, 'locations_data.csv')
+adverts_csv_name <- file.path(data_folder, 'all_full_advert_data.csv')
+
+
